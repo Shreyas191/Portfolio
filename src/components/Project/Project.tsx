85c2af58d68314392e901410361afa8b818d1bba
Shreyas <shreyaskaldate02@gmail.com>
2025-04-27 11:44:36 +0530

    github links updated

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -61,7 +61,7 @@ export function Project() {
                 <path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path>
               </svg>
               <div className="project-links">
-                <a href="https://github.com/Shreyas191/E-Commerce-APIs" target="\_blank" rel="noreferrer">
+                <a href="https://github.com/Shreyas191/Smart-Contact-Manager" target="\_blank" rel="noreferrer">
                   <img src={githubIcon} alt="Visit site" />
                 </a>
               </div>
@@ -87,7 +87,7 @@ export function Project() {
             <header>
               <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
               <div className="project-links">
-                <a href="https://github.com/Shreyas191/Final-Year-Project" target="_blank" rel="noreferrer">
+                <a href="https://github.com/Shreyas191/Smart-Irrigation-and-Worm-Detection" target="_blank" rel="noreferrer">
                   <img src={githubIcon} alt="Visit site" />
                 </a>
               </div>
